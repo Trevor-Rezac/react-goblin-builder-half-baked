a44d910c9dac69586e2f093527b2bcc611109166
TrevorRezac <trev.rezac@gmail.com>
2022-02-02 18:56:34 -0800

    delete function works but not on filtered goblins

--- src/Goblin.js
+++ src/Goblin.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-export default function Goblin({ goblin }) {
+export default function Goblin({ goblin, handleDeleteGoblin }) {
   return (
     // be sure you take a look at this component i'm handing you and figure out what props it will need to work correctly.
     <div 
       className='goblin' 
-      // onClick={() => handleDeleteGoblin && handleDeleteGoblin(goblin.id)}
+      onClick={() => handleDeleteGoblin && handleDeleteGoblin(goblin.id)}
     >
       <h3>{goblin.name}</h3>  
       <img src="goblin.png" style={{ backgroundColor: goblin.color }} />
